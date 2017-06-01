98567fc025b879c42a979f6c3f20573dc6fdffa8
Jim X. Lin <miljinx@gmail.com>
2017-05-31 23:20:58 -0700

    Add todos and items controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,7 @@ end
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
